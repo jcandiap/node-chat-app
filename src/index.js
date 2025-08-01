f8016bc90a77ecffab798aa47a5c3a863f165db2
Jose Candia <josecandiap@gmail.com>
2025-08-01 10:37:13 -0400

    fix: files sizes and endpoint

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 const port = process.env.PORT || 5001;
 
-app.use(express.json());
+app.use(express.json({ limit: '5mb' }));
 app.use(cookieParser());
 
 app.use(cors({
@@ -21,7 +21,7 @@ app.use(cors({
 }));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
