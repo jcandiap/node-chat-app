49e08bab8d6333df2c2a25b992a42e3784f87a30
Jose Candia <josecandiap@gmail.com>
2025-08-22 23:37:52 -0400

    feat: adding socket connection

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 
+import { app, io, server } from "./lib/socket.js";
+
 dotenv.config();
-const app = express();
 
 const port = process.env.PORT || 5001;
 
@@ -23,7 +24,7 @@ app.use(cors({
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server running on port ${port}`);
     connectDB();
 })
